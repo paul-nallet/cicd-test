8055e00c830a8738acfd369f20e53fff6d17bc3d
Nallet <pnallet@pageup.fr>
2022-01-28 19:57:16 +0100

    new secrets

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,7 +9,22 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+<<<<<<< HEAD
     use AuthorizesRequests;
     use DispatchesJobs;
     use ValidatesRequests;
+=======
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function test()  :void
+    {
+
+    }
+
+    public function mange() :string
+    {
+                return "bojour";
+
+    }
+>>>>>>> a181553 (new secrets)
 }
